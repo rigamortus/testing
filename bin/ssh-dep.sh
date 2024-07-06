b50de3ad5415cc11669a75f12471a19e46fccb08
Akalugo David <109105332+rigamortus@users.noreply.github.com>
2024-07-06 11:05:35 +0100

    Update ssh-dep.sh

--- bin/ssh-dep.sh
+++ bin/ssh-dep.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 port=${port:-80}
-prod="${shipit_prod_user:-$shipit_prod_user}"@"${shipit_prod_host:-$shipit_prod_host}"
-staging="${shipit_staging_user:-$shipit_staging_user}"@"${shipit_staging_host:-$shipit_staging_host}"
+prod="${shipit_prod_user:-$SHIPIT_PROD_USER}"@"${shipit_prod_host:-$$SHIPIT_PROD_HOST}"
+staging="${shipit_staging_user:-$SHIPIT_STAGING_USER}"@"${shipit_staging_host:-$SHIPIT_STAGING_HOST}"
 image=${image:-rigamortus/newdocker234:15}
 if [[ "$target" = "staging" ]]; then
     targetEnv="$staging"
